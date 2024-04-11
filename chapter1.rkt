0169291f5c77d3f37bd35de6b2924f1bd73b7796
ZhangHao <zhanghao@antigen.top>
2024-04-11 12:07:05 +0800

    Enforce data abstraction

--- chapter1.rkt
+++ chapter1.rkt
@@ -1,9 +1,11 @@
 #lang racket/base
-(require racket/contract)
+(require racket/contract racket/generic)
 (provide (contract-out (rename remq remove-first (-> symbol? (listof symbol?) (listof symbol?)))
-                       (occurs-free? (-> symbol? LcExp? boolean?))
+                       (occurs-free? (-> variable? LcExp? boolean?))
                        (subst (-> symbol? symbol? (listof s-exp?) (listof s-exp?))))
-         LcExp? s-exp?
+         s-exp?
+         gen:LcExp-datum LcExp-datum/c LcExp-datum?
+         variable? lambda? app? LcExp? variable-symbol lambda-symbol lambda-body app-operand app-operator
          (rename-out (length list-length)
                      (list-ref nth-element)))
 
@@ -15,27 +17,63 @@
   (or (and (zero? n) (null? l))
       (and (not (null? l)) (list-of-length/unsafe? (cdr l) (sub1 n)))))
 
+;; LcExp Interfaces
 #; (-> any/c boolean?)
-;; Recognizes lambda-calculus expressions
-(define (LcExp? v)
-  (or
-   (symbol? v)
-   (and (list-of-length? v 3)
-        (eq? (car v) 'lambda)
-        (list-of-length? (cadr v) 1)
-        (symbol? (caadr v))
-        (LcExp? (caddr v)))
-   (and (list-of-length? v 2)
-        (andmap LcExp? v))))
+;; Predicates for the default representation
+(define (maybe-default-LcExp? v) (or (symbol? v) (list? v)))
+(define (default-variable? v) (symbol? v))
+(define (default-lambda? v)
+  (and (list-of-length? v 3)
+       (eq? (car v) 'lambda)
+       (list-of-length? (cadr v) 1)
+       (symbol? (caadr v))
+       (default-LcExp? (caddr v))))
+(define (default-app? v)
+  (and (list-of-length? v 2)
+       (andmap default-LcExp? v)))
+(define (default-LcExp? v)
+  (or (default-variable? v)
+      (default-lambda? v)
+      (default-app? v)))
+;; Fallback predicates
+(define (fallback-LcExp? v)
+  (or (variable? v) (lambda? v) (app? v)))
+;; Defines gen:LcExp-datum
+(define-generics LcExp-datum
+  [variable? LcExp-datum]
+  [lambda? LcExp-datum]
+  [app? LcExp-datum]
+  [LcExp? LcExp-datum]
+  [variable-symbol LcExp-datum]
+  [lambda-symbol LcExp-datum]
+  [lambda-body LcExp-datum]
+  [app-operand LcExp-datum]
+  [app-operator LcExp-datum]
+  #:fallbacks [(define LcExp? fallback-LcExp?)]
+  #:fast-defaults ([maybe-default-LcExp?
+                    (define variable? default-variable?)
+                    (define lambda? default-lambda?)
+                    (define app? default-app?)
+                    (define LcExp? default-LcExp?)
+                    (define (variable-symbol v) v)
+                    (define lambda-symbol caadr)
+                    (define lambda-body caddr)
+                    (define app-operand cadr)
+                    (define app-operator car)
+                    ]))
 
-#; (-> symbol? LcExp? boolean?)
-;; Returns #t if the symbol occurs free in the LcExp, otherwise returns #f
-(define (occurs-free? sym expr)
-  (cond ((symbol? expr) (eq? sym expr))
-        ((list-of-length? expr 3)
-         (and (not (eq? sym (caadr expr)))
-              (occurs-free? sym (caddr expr))))
-        (else (ormap (lambda (e) (occurs-free? sym e)) expr))))
+#; (-> variable? LcExp? boolean?)
+;; Returns #t if the variable occurs free in the LcExp, otherwise returns #f
+(define (occurs-free? var expr)
+
+  (define (occurs-free/sym? sym expr)
+    (cond ((variable? expr) (eq? sym (variable-symbol expr)))
+          ((lambda? expr)
+           (and (not (eq? sym (lambda-symbol expr)))
+                (occurs-free/sym? sym (caddr expr))))
+          (else (ormap (lambda (e) (occurs-free/sym? sym e)) expr))))
+
+  (occurs-free/sym? (variable-symbol var) expr))
 
 #; (-> any/c boolean?)
 ;; Recognizes s-exp
@@ -58,7 +96,7 @@
   (check-true (LcExp? '(lambda (v) v)))
   (check-true (LcExp? 'v))
   (check-true (LcExp? '((lambda (v) v) (lambda (v) v))))
-  (check-false (LcExp? 1))
+  (check-exn exn:fail:contract? (lambda () (LcExp? 1)))
   (check-true (occurs-free? 'v 'v))
   (check-false (occurs-free? 'v '(lambda (v) v)))
   (check-true (occurs-free? 'v '(lambda (x) v)))
